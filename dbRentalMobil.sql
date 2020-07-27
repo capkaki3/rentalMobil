e9795560ef392dcb6e03fb2a3e5f9049fe240b18
kamalfikrikamal <58419580+kamalfikrikamal@users.noreply.github.com>
2020-07-27 09:00:56 +0700

    Add files via upload

--- dbRentalMobil.sql
+++ dbRentalMobil.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jul 27, 2020 at 03:35 AM
+-- Generation Time: Jul 26, 2020 at 06:06 PM
 -- Server version: 10.3.16-MariaDB
 -- PHP Version: 7.3.6
 
@@ -32,7 +32,7 @@ CREATE TABLE `karyawan` (
   `NIK` int(20) NOT NULL,
   `Nama` varchar(50) NOT NULL,
   `Alamat` varchar(150) NOT NULL,
-  `notelpon` varchar(30) NOT NULL
+  `notelpon` int(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -40,33 +40,7 @@ CREATE TABLE `karyawan` (
 --
 
 INSERT INTO `karyawan` (`NIK`, `Nama`, `Alamat`, `notelpon`) VALUES
-(1234567, 'jojon', 'depok', '089878767656'),
-(1234568, 'amin', 'bogor', '08789878723');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `kendaraan`
---
-
-CREATE TABLE `kendaraan` (
-  `noPlat` varchar(20) NOT NULL,
-  `namaKendaraan` varchar(50) NOT NULL,
-  `tahun` varchar(20) NOT NULL,
-  `Tarif` varchar(10) NOT NULL,
-  `statusRental` varchar(10) NOT NULL,
-  `namaPemilik` varchar(50) NOT NULL,
-  `tipeKendaraan` varchar(20) NOT NULL,
-  `merekKendaraan` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `kendaraan`
---
-
-INSERT INTO `kendaraan` (`noPlat`, `namaKendaraan`, `tahun`, `Tarif`, `statusRental`, `namaPemilik`, `tipeKendaraan`, `merekKendaraan`) VALUES
-('B12345HH', 'jaka', '2010', '3000000', 'Tidak', 'Toyota', 'B123', 'ikhsan gustya'),
-('B2345KK', 'andi', '2017', '2300000', 'Tidak', 'Mitsubitsi', 'B123', 'muhammad fikri kamal');
+(88, 'abc', 'jjj', 888);
 
 -- --------------------------------------------------------
 
@@ -86,58 +60,9 @@ CREATE TABLE `login` (
 --
 
 INSERT INTO `login` (`id`, `username`, `password`, `role`) VALUES
-(1, 'kamal1', 'kamal1', 'Admin'),
-(2, 'iang1', 'iang1', 'Admin'),
-(3, 'yafir1', 'yafir1', 'Admin'),
-(4, 'iang2', 'iang2', 'Karyawan'),
-(5, 'kamal2', 'kamal2', 'Karyawan'),
+(1, 'kamal2', 'kamal2', 'Users'),
 (7, 'yafir2', 'yafir2', 'Karyawan'),
-(8, 'admin', 'admin', 'Admin'),
-(9, 'user', 'user', 'Karyawan');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `merekKendaraan`
---
-
-CREATE TABLE `merekKendaraan` (
-  `idMerek` int(10) NOT NULL,
-  `Nama` varchar(50) NOT NULL,
-  `Keterangan` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `merekKendaraan`
---
-
-INSERT INTO `merekKendaraan` (`idMerek`, `Nama`, `Keterangan`) VALUES
-(1, 'Toyota', 'Hitam'),
-(2, 'Suzuki', 'Putih'),
-(3, 'Honda', 'Merah'),
-(4, 'Mitsubitsi', 'Abu-abu');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `pelanggan`
---
-
-CREATE TABLE `pelanggan` (
-  `noKtp` int(20) NOT NULL,
-  `Nama` varchar(50) NOT NULL,
-  `Alamat` varchar(150) NOT NULL,
-  `notelpon` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `pelanggan`
---
-
-INSERT INTO `pelanggan` (`noKtp`, `Nama`, `Alamat`, `notelpon`) VALUES
-(1234789, 'pak jaya', 'jakarta', '085678123456'),
-(1234798, 'pak budi', 'tangerang', '089676345234'),
-(1234876, 'pak sigit', 'serang', '085678456675');
+(18, 'iang', 'iang', 'Karyawan');
 
 -- --------------------------------------------------------
 
@@ -149,7 +74,7 @@ CREATE TABLE `pemilik` (
   `noKtp` int(20) NOT NULL,
   `Nama` varchar(50) NOT NULL,
   `Alamat` varchar(150) NOT NULL,
-  `notelpon` varchar(15) NOT NULL
+  `notelpon` int(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -157,67 +82,194 @@ CREATE TABLE `pemilik` (
 --
 
 INSERT INTO `pemilik` (`noKtp`, `Nama`, `Alamat`, `notelpon`) VALUES
-(321232, 'ikhsan gustya', 'depok', '087676567765'),
-(321233, 'muhammad fikri kamal', 'jakarta', '087676545454'),
-(321231, 'iang nugraha', 'bogor', '085676787898');
+(777, 'aaaa', 'ba', 222),
+(44, 'yy', 'ddd', 555);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `sopir`
+-- Table structure for table `role`
 --
 
-CREATE TABLE `sopir` (
-  `noSim` int(20) NOT NULL,
-  `Nama` varchar(50) NOT NULL,
-  `Alamat` varchar(150) NOT NULL,
-  `notelpon` varchar(15) NOT NULL,
-  `tarif` varchar(20) NOT NULL,
-  `statusRental` varchar(30) NOT NULL
+CREATE TABLE `role` (
+  `id` int(10) NOT NULL,
+  `user` int(10) NOT NULL,
+  `admin` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `sopir`
---
-
-INSERT INTO `sopir` (`noSim`, `Nama`, `Alamat`, `notelpon`, `tarif`, `statusRental`) VALUES
-(987654323, 'udin', 'bogor', '087876565432', '2000000', 'Ya'),
-(987654333, 'mamat', 'jakarta', '089765432345', '2000000', 'Tidak'),
-(987654345, 'rojak', 'cibinong', '087878676543', '2300000', 'Tidak');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tipeKendaraan`
+-- Table structure for table `tbadmin`
 --
 
-CREATE TABLE `tipeKendaraan` (
-  `idTipe` int(10) NOT NULL,
-  `Nama` varchar(50) NOT NULL,
-  `Keterangan` varchar(100) NOT NULL
+CREATE TABLE `tbadmin` (
+  `kd_admin` varchar(10) NOT NULL,
+  `nama_admin` varchar(20) NOT NULL,
+  `tlp_admin` varchar(20) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(300) NOT NULL,
+  `level` enum('admin','pemilik') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tipeKendaraan`
+-- Dumping data for table `tbadmin`
 --
 
-INSERT INTO `tipeKendaraan` (`idTipe`, `Nama`, `Keterangan`) VALUES
-(1, 'A123', 'Keluarga'),
-(2, 'B123', 'Bisnis'),
-(3, 'C123', 'Umum');
+INSERT INTO `tbadmin` (`kd_admin`, `nama_admin`, `tlp_admin`, `username`, `password`, `level`) VALUES
+('1', 'Fikri Kamal', '085659456882', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin'),
+('2', 'yaafir', '08283753123', 'pemilik', '58399557dae3c60e23c78606771dfa3d', 'pemilik');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `transaksi`
+-- Table structure for table `tbcostumer`
 --
 
-CREATE TABLE `transaksi` (
-  `idTransaksi` int(10) NOT NULL,
-  `noPlat` varchar(10) NOT NULL,
-  `namaKendaraan` varchar(50) NOT NULL,
-  `tarif` varchar(10) NOT NULL
+CREATE TABLE `tbcostumer` (
+  `kd_cs` int(10) NOT NULL,
+  `nama_cs` varchar(50) NOT NULL,
+  `kelamin_cs` varchar(20) NOT NULL,
+  `alamat_cs` varchar(20) NOT NULL,
+  `tlp_cs` varchar(20) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(300) NOT NULL,
+  `level_cs` enum('user') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbcostumer`
+--
+
+INSERT INTO `tbcostumer` (`kd_cs`, `nama_cs`, `kelamin_cs`, `alamat_cs`, `tlp_cs`, `username`, `password`, `level_cs`) VALUES
+(1, 'rizky', 'Laki-Laki', 'Kp. Karajeun II', '085659456197', 'rizky', '49d8712dd6ac9c3745d53cd4be48284c', 'user'),
+(2, 'bule', 'Laki-Laki', 'kaswari', '989829352', 'bule', '90043f6fdf95e16dc1c5d43cb4f501e7', 'user'),
+(3, 'kamal', 'pria', 'bogor', '08878787878', 'kamal', 'kamal', 'user');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbdetailsewa`
+--
+
+CREATE TABLE `tbdetailsewa` (
+  `kd_detail` varchar(20) NOT NULL,
+  `kd_cs` varchar(20) NOT NULL,
+  `kd_sewa` varchar(20) NOT NULL,
+  `kd_sopir` varchar(20) NOT NULL,
+  `total_harga` varchar(20) NOT NULL,
+  `ket` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbdetailsewa`
+--
+
+INSERT INTO `tbdetailsewa` (`kd_detail`, `kd_cs`, `kd_sewa`, `kd_sopir`, `total_harga`, `ket`) VALUES
+('D01', '1', 'W01', 'S01', '1000000', 'Sudah di Konfirmasi'),
+('D02', '2', 'W02', 'S03', '1470000', 'Belum di Konfirmasi'),
+('D03', '1', 'W03', 'S02', '1150000', 'Belum di Konfirmasi');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbmerk`
+--
+
+CREATE TABLE `tbmerk` (
+  `kd_merk` varchar(10) NOT NULL,
+  `merk_mobil` varchar(20) NOT NULL,
+  `jenis_mobil` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbmerk`
+--
+
+INSERT INTO `tbmerk` (`kd_merk`, `merk_mobil`, `jenis_mobil`) VALUES
+('K01', 'Toyota', 'Avanza'),
+('K02', 'Honda', 'CRV'),
+('K03', 'Honda', 'HRV'),
+('K04', 'Toyota', 'Alphard'),
+('K05', 'Toyota', 'Fortuner'),
+('K06', 'Toyota', 'Rush'),
+('K07', 'honda', 'hrv'),
+('K08', 'Daihatsu', 'Xenia');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbmobil`
+--
+
+CREATE TABLE `tbmobil` (
+  `no_polisi` varchar(20) NOT NULL,
+  `kd_merk` varchar(10) NOT NULL,
+  `foto_mobil` varchar(300) NOT NULL,
+  `tarif_mobil` varchar(20) NOT NULL,
+  `keterangan` varchar(20) NOT NULL,
+  `status` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbmobil`
+--
+
+INSERT INTO `tbmobil` (`no_polisi`, `kd_merk`, `foto_mobil`, `tarif_mobil`, `keterangan`, `status`) VALUES
+('F 12345 OP', 'K01', '2018-02-17-toyota avanza.jpg', '300000', 'Bagus', 'Tersedia'),
+('F 2300 MN', 'K06', '2018-02-17-toyota rush.jpg', '500000', 'Bagus', 'Tersedia'),
+('F 7543 LA', 'K04', '2018-02-17-toyota alphard.jpg', '700000', 'Bagus', 'Tidak Tersedia'),
+('F 7591 AB', 'K05', '2018-02-17-toyota fortuner.jpg', '550000', 'Bagus', 'Tidak Tersedia'),
+('F 7629 KI', 'K02', '2018-02-17-honda crv.jpg', '450000', 'Bagus', 'Tidak Tersedia'),
+('F 9021 BI', 'K03', '2018-02-17-honda hrv.jpg', '500000', 'Bagus', 'Tersedia');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbsewa`
+--
+
+CREATE TABLE `tbsewa` (
+  `kd_sewa` varchar(10) NOT NULL,
+  `no_polisi` varchar(10) NOT NULL,
+  `tgl_sewa` date NOT NULL,
+  `tgl_kembali` date NOT NULL,
+  `total_sewa` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbsewa`
+--
+
+INSERT INTO `tbsewa` (`kd_sewa`, `no_polisi`, `tgl_sewa`, `tgl_kembali`, `total_sewa`) VALUES
+('W01', 'F 7629 KI', '2018-02-01', '2018-02-03', '900000'),
+('W02', 'F 7543 LA', '2018-03-01', '2018-03-03', '1400000'),
+('W03', 'F 7591 AB', '2018-02-20', '2018-02-22', '1100000');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbsopir`
+--
+
+CREATE TABLE `tbsopir` (
+  `kd_sopir` varchar(20) NOT NULL,
+  `nama_sopir` varchar(20) NOT NULL,
+  `kelamin_sopir` varchar(20) NOT NULL,
+  `umur_sopir` varchar(10) NOT NULL,
+  `tlp_sopir` varchar(30) NOT NULL,
+  `alamat_sopir` varchar(100) NOT NULL,
+  `tarif_sopir` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbsopir`
+--
+
+INSERT INTO `tbsopir` (`kd_sopir`, `nama_sopir`, `kelamin_sopir`, `umur_sopir`, `tlp_sopir`, `alamat_sopir`, `tarif_sopir`) VALUES
+('S01', 'Qodir', 'Laki - Laki', '25 Tahun', '085676548091', 'Kp. Rambutan Aceh Tapi Manis', '100000'),
+('S02', 'Wan Qodir', 'Laki - Laki', '30 Tahun', '089076543287', 'Kp. Tipar Gede', '50000'),
+('S03', 'Dewi Yuli Dew', 'Perempuan', '26', '098056743021', 'Kp. Legok Hangser', '70000');
 
 --
 -- Indexes for dumped tables
@@ -230,6 +282,30 @@ ALTER TABLE `login`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbadmin`
+--
+ALTER TABLE `tbadmin`
+  ADD PRIMARY KEY (`kd_admin`);
+
+--
+-- Indexes for table `tbcostumer`
+--
+ALTER TABLE `tbcostumer`
+  ADD PRIMARY KEY (`kd_cs`);
+
+--
+-- Indexes for table `tbmobil`
+--
+ALTER TABLE `tbmobil`
+  ADD PRIMARY KEY (`no_polisi`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -238,6 +314,18 @@ ALTER TABLE `login`
 --
 ALTER TABLE `login`
   MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `role`
+--
+ALTER TABLE `role`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbcostumer`
+--
+ALTER TABLE `tbcostumer`
+  MODIFY `kd_cs` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
